c562c044329cee23d865039ac4c4c135291c5a44
Gabor Szabo <gabor@szabgab.com>
2023-07-28 11:06:01 +0300

    autocommit

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut x = 1;
-    dbg!(x);
-    x = x + 1;
-    dbg!(x);
+    //let mut x = 1;
+    //dbg!(x);
+    //x = x + 1;
+    //dbg!(x);
+
+    let age = "23";
+    dbg!(age);
+    let age = 23;
+    dbg!(age);
 }
