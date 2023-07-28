03fdd1986d785a4f7bde1b818b01b6400fbb00b5
Gabor Szabo <gabor@szabgab.com>
2023-07-28 10:20:01 +0300

    autocommit

--- numbers/src/main.rs
+++ numbers/src/main.rs
@@ -2,18 +2,18 @@ fn main() {
     let a = 23;
     //let b: i16 = 10;
     //let c = 2.3;
-    let b = 19;
-    let c = a + b;
-    dbg!(c);
-    let d = a / b; // integer based
-    dbg!(d);
+    // let b = 19;
+    // let c = a + b;
+    // dbg!(c);
+    // let d = a / b; // integer based
+    // dbg!(d);
 
-    // let f = a / b as f32; // this does not work
-    let f = a as f32 / b as f32;
-    dbg!(f);
-
+    // // let f = a / b as f32; // this does not work
+    // let f = a as f32 / b as f32;
+    // dbg!(f);
+    // hello();
 }
 
-fn hello() {
-    println!("Hello World");
-}
+// fn hello() {
+//     println!("Hello World");
+// }
